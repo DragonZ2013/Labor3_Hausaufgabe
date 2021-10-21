bd5a2bcb8deb5f46aa389f1c11e1a95b2eece290
DragonZ <drag.popescu@yahoo.com>
2021-10-21 14:44:28 +0300

    Getter/Setter tests

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,7 +15,11 @@ public class Main {
         sportList.add(sport2);
         sportList.add(sport3);
         sportList.add(sport4);
-        Benutzer user1 = new Benutzer("Popescu","Dragos",sportList);
+        Benutzer user1 = new Benutzer("Emil","Robert",sportList);
+        System.out.println(user1.getVorName()+" "+user1.getNachName());
+        user1.setNachName("Pop");
+        user1.setVorName("Radu");
+        System.out.println(user1.getVorName()+" "+user1.getNachName());
         System.out.println(user1.kalkuliereZeit());
         System.out.println(user1.kalkuliereZeit(sport2));
         System.out.println(user1.kalkuliereDurschnittszeit());
